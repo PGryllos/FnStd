de8ab5ea98ed07e9fd8d5d1acbb9f50cdea65739
Gryllos Prokopis <gryllosprokopis@gmail.com>
2015-04-16 02:53:05 +0300

    Add average time calculation

--- MedianList/medianTest.cc
+++ MedianList/medianTest.cc
@@ -1,19 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+#include <vector>
 #include "median.h"
+
+#define ITER 10000
 
 
 using namespace std;
 
-int main () 
+clock_t insertTime = 0, deleteTime = 0, starting_time;
+
+void printAverageTime()
 {
+    cout<<"Average insertion time: "
+        <<((double)insertTime/ITER)/CLOCKS_PER_SEC<<endl;
+    cout<<"Average deletion time: "
+        <<((double)deleteTime/ITER)/CLOCKS_PER_SEC<<endl;
+}
+
+int main (void)
+{
+
+    vector<int> values;
+
     srand (time(NULL));
-    MedianList a;
-    for (int i=0; i<100000; i++)
+    MedianList container;
+
+    /* Insert values to container and calculate insertion time */
+    for (int i=0; i<ITER; i++)
     {
-        int r = rand() % 100000 + 0;
-        a.insert(r);
+        int r = rand() % 10000 + 137;
+        values.push_back(r);
+
+        starting_time = clock();
+
+        container.insert(r);
+
+        insertTime += clock() - starting_time;
     }
-    cout<<a.getMedian();
+
+    cout<<"median: "<<container.getMedian()<<endl;
+
+    /* Delete all values from container and calculate deletion time */
+    while (!values.empty())
+    {
+        starting_time = clock();
+
+        container.remove(values.back());
+
+        deleteTime += clock() - starting_time;
+        values.pop_back();
+    }
+
+    printAverageTime();
+
     return 0;
 }
